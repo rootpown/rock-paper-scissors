478bcf0c22d94032052b694df1c49fcd79b99b74
rootpown <rootpown@gmail.com>
2025-07-15 12:17:49 +0400

    выбор CPU

--- script.js
+++ script.js
@@ -11,10 +11,39 @@ const countPlayer = document.getElementById('countPlayer');
 const countCPU = document.getElementById("countCPU")
 
 // выбор карты => отображение
-const resultModel = getElementById('elems');
+// const resultModel = getElementById('elems');
 
 // выбор блока
 
 const paper = document.getElementById('paper');
 const rock = document.getElementById('rock');
 const scissors = document.getElementById('scissors');
+// paper.addEventListener('click', () => {
+//     let models = document.createElement('div')
+//     models.id = 'elems'
+//     modelPlayer.append(models)
+// })
+//  Рандомный выбор CPU 
+//  ВЫбор Player
+//  Сравнение.
+function getChoiceCPU() {
+    let models = document.createElement('div')
+    let randomChoice = Math.ceil(Math.random() * 3)
+    if ( randomChoice === 1) {
+            models.id = 'block_rock'
+            modelCPU.append(models)
+            return 'ROCK'
+    } else if ( randomChoice === 2) { 
+            models.id = 'block_paper'
+            modelCPU.append(models)
+            return 'PAPER'
+    } else if ( randomChoice === 3) { 
+            models.id = 'block_scissors'
+            modelCPU.append(models)
+            return 'SCISSORS'
+    } else { 
+        return undefined
+    }
+}
+function getGameData() { 
+} 
