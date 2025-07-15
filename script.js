b5b9ad916da5cb21f67b028b616b9e8b0c3ca63a
rootpown <rootpown@gmail.com>
2025-07-15 11:17:49 +0400

    Добавил объекты DOM в js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// status_game
-// restart_game
-// block_modelPlayer
-// block_modelCPU
-// elems <= 'result block'
-// countPlayer
-// countCPU
-// paper
-// rock
-// scissors
+// модель.окно
+const modelWindow = document.getElementById("modal_window");
+const getStatusGame = document.getElementById('status_game')
+const restartGame = document.getElementById('restart_game')
+
+// блоки игроков.
+const modelPlayer = document.getElementById('block_modelPlayer');
+const modelCPU = document.getElementById('block_modelCPU');
+// счет игроков
+const countPlayer = document.getElementById('countPlayer');
+const countCPU = document.getElementById("countCPU")
+
+// выбор карты => отображение
+const resultModel = getElementById('elems');
+
+// выбор блока
+
+const paper = document.getElementById('paper');
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
